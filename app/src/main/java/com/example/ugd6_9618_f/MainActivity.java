c2488fe8e29376d22b8f9b45a11efe7fee69a6f6
rastrakidyana <rastrakidyana@users.noreply.github.com>
2020-10-02 20:37:35 +0800

    master file 2

--- app/src/main/java/com/example/ugd6_9618_f/MainActivity.java
+++ app/src/main/java/com/example/ugd6_9618_f/MainActivity.java
@@ -10,17 +10,24 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
 import com.mapbox.api.geocoding.v5.models.CarmenFeature;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -36,25 +43,39 @@ import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
 import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener {
 
     private static final String DESTINATION_SYMBOL_LAYER_ID = "destination-symbol-layer-id";
     private static final String DESTINATION_ICON_ID = "destination-icon-id";
     private static final String DESTINATION_SOURCE_ID = "destination-source-id";
     private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
+    private static final String TAG = "MainActivity";
     private FloatingActionButton searchfab;
+    private LocationComponent locationComponent;
     private PermissionsManager permissionsManager;
+    private DirectionsRoute currentRoute;
+    private NavigationMapRoute navigationMapRoute;
     private MapboxMap mapboxMap;
     private MapView mapView;
     private Point origin;
+    private Point destination;
+    private Button start_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +89,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         searchfab = findViewById(R.id.fab_location_search);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+
 
         searchfab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,22 +120,72 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         loadedMapStyle.addLayer(destinationSymbolLayer);
     }
 
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+
+        destination = Point.fromLngLat(point.getLongitude(), point.getLatitude());
+        origin = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
+                locationComponent.getLastKnownLocation().getLatitude());
+
+        GeoJsonSource source = mapboxMap.getStyle().getSourceAs("destination-source-id");
+        if (source != null) {
+            source.setGeoJson(Feature.fromGeometry(destination));
+        }
+
+        getRoute(origin, destination);
+        start_btn.setEnabled(true);
+        start_btn.setBackgroundResource(R.color.mapbox_blue);
+        return true;
+    }
+
+
+
+    private void getRoute(Point origin, Point destination) {
+        NavigationRoute.builder(this)
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        // You can get the generic HTTP info about the response
+                        Log.d(TAG, "Response code: " + response.code());
+                        if (response.body() == null) {
+                            Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+                            return;
+                        } else if (response.body().routes().size() < 1) {
+                            Log.e(TAG, "No routes found");
+                            return;
+                        }
+
+                        currentRoute = response.body().routes().get(0);
+
+                        if (navigationMapRoute != null) {
+                            navigationMapRoute.removeRoute();
+                        } else {
+                            navigationMapRoute = new NavigationMapRoute(null, mapView, mapboxMap, R.style.NavigationMapRoute);
+                        }
+                        navigationMapRoute.addRoute(currentRoute);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+                        Log.e(TAG, "Error: " + throwable.getMessage());
+                    }
+                });
+    }
+
+
     @SuppressLint("MissingPermission")
     private void enableLocationComponent(@NonNull Style loadedMapStyle) {
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
-            LocationComponent locationComponent = mapboxMap.getLocationComponent();
-
-            locationComponent.activateLocationComponent(
-                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build()
-            );
+            locationComponent = mapboxMap.getLocationComponent();
+            locationComponent.activateLocationComponent(this, loadedMapStyle);
 
             locationComponent.setLocationComponentEnabled(true);
-
             locationComponent.setCameraMode(CameraMode.TRACKING);
-
             locationComponent.setRenderMode(RenderMode.COMPASS);
-            this.origin = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
-                    locationComponent.getLastKnownLocation().getLatitude());
         } else {
             permissionsManager = new PermissionsManager(this);
             permissionsManager.requestLocationPermissions(this);
@@ -129,7 +201,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             if(mapboxMap != null){
                 Style style = mapboxMap.getStyle();
                 if(style != null){
-                    GeoJsonSource source = style.getSourceAs("geojsonSourceLayerid");
+                    GeoJsonSource source = style.getSourceAs("destination-source-id");
                     if(source != null){
                         source.setGeoJson(FeatureCollection.fromFeatures(
                                 new Feature[] {
@@ -144,6 +216,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                                     .zoom(14)
                                     .build()), 4000);
 
+
+                    destination = Point.fromLngLat( ((Point) selectedCarmenFeature.geometry()).longitude(), ((Point) selectedCarmenFeature.geometry()).latitude() );
+                    origin = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
+                            locationComponent.getLastKnownLocation().getLatitude());
+
+                    getRoute(origin, destination);
+                    start_btn.setEnabled(true);
+                    start_btn.setBackgroundResource(R.color.mapbox_blue);
                 }
             }
         }
@@ -175,30 +255,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
-        mapboxMap.setStyle(new Style.Builder().fromUri(Style.MAPBOX_STREETS),
-                new Style.OnStyleLoaded() {
+        mapboxMap.setStyle(getString(R.string.navigation_guidance_day), new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                enableLocationComponent(style);
+
+               initLayers(style);
+
+                mapboxMap.addOnMapClickListener(MainActivity.this);
+
+                start_btn = findViewById(R.id.btn_nav);
+
+                start_btn.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onStyleLoaded(@NonNull Style style){
-                        List<Feature> symbolLayerIconFeatureList = new ArrayList<>();
-                        enableLocationComponent(style);
-
-                        initLayers(style);
-                        mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
-                            @Override
-                            public boolean onMapClick(@NonNull LatLng point){
-                                symbolLayerIconFeatureList.add(Feature.fromGeometry(
-                                        Point.fromLngLat(point.getLongitude(), point.getLatitude())));
-
-                                GeoJsonSource source = mapboxMap.getStyle().getSourceAs(DESTINATION_SOURCE_ID);
-                                source.setGeoJson(FeatureCollection.fromFeatures(symbolLayerIconFeatureList));
-
-                                return true;
-                            }
-                        });
+                    public void onClick(View v) {
+                        boolean simulateRoute = true;
+                        NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                                .directionsRoute(currentRoute)
+                                .shouldSimulateRoute(simulateRoute)
+                                .build();
+                        NavigationLauncher.startNavigation(MainActivity.this, options);
                     }
                 });
+            }
+        });
     }
 
 
@@ -236,6 +318,41 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_map_style, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.streets:
+                mapboxMap.setStyle(Style.MAPBOX_STREETS);
+                return true;
+            case R.id.dark:
+                mapboxMap.setStyle(Style.DARK);
+                return true;
+            case R.id.light:
+                mapboxMap.setStyle(Style.LIGHT);
+                return true;
+            case R.id.traffic_day:
+                mapboxMap.setStyle(Style.TRAFFIC_DAY);
+                return true;
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
 
